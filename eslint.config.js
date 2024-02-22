ecb1865232efe626278e59f0475a61e6b4ad2ebd
neoki <ot07neoki@gmail.com>
2024-02-23 02:24:18 +0900

    chore: update quality check config

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,22 @@
 import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-import tsEsLintPlugin from '@typescript-eslint/eslint-plugin'
-import tsEsLintParser from '@typescript-eslint/parser'
+import ts from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import globals from 'globals'
-
-const compat = new FlatCompat()
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
   { ignores: ['node_modules', 'dist'] },
   js.configs.recommended,
   eslintConfigPrettier,
-  ...compat.extends(
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ),
-  {
+  ...ts.config(...ts.configs.recommended, {
     files: ['**/*.ts'],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
       globals: { ...globals.browser },
-      parser: tsEsLintParser,
       parserOptions: {
-        project: './tsconfig.eslint.json',
+        project: './tsconfig.quality.json',
       },
     },
-    plugins: {
-      tsEsLintPlugin,
-    },
-  },
+  }),
 ]
